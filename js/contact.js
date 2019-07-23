82ff4458d87a9f7b8c9f1ee3adf954f7446fb6df
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-07-24 01:29:09 +0200

    Update contact form

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,22 @@
 $(window).on("load", function () {
   $('#mail-form').on('submit', function (e) {
     e.preventDefault();
-    console.log(e);
+    console.log($(this).serialize());
+    var form = $(this);
+    $.ajax({
+      method: 'GET',
+      url: 'http://127.0.0.1/carlgo11/new-site/mail/mail.php',
+      dataType: "text",
+      cache: false,
+      data: form.serialize(),
+      success: function () {
+        $('#form-success').fadeIn().alert();
+        $('#mail-form').hide();
+
+      },
+      error: function () {
+        $('#form-error').fadeIn().alert();
+      }
+    });
   });
 });
