2b9d18b909645ff67cb17bcb861171bdf0c9b7b5
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-07-26 02:18:42 +0200

    Preload things & add documentation

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,29 @@
 $(window).on("load", function () {
+
+  /* On form submit */
   $('#mail-form').on('submit', function (e) {
     e.preventDefault();
     console.log($(this).serialize());
     var form = $(this);
+
+    /* Send form data */
     $.ajax({
       method: 'GET',
       url: 'https://submit.carlgo11.com/',
       dataType: "text",
       cache: false,
       data: form.serialize(),
+
       success: function () {
         $('#form-success').fadeIn().alert();
         $('#mail-form').hide();
-
       },
+
       error: function () {
         $('#form-error').fadeIn().alert();
       }
     });
+
   });
+
 });
