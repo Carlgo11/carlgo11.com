7fb495855f5a412e3310ebf73e814be65649becf
Carlgo11 <carlgo11@users.noreply.github.com>
2024-02-28 22:27:53 +0100

    Update contact form

--- js/contact.js
+++ js/contact.js
@@ -1,18 +1,32 @@
-$(window).on('load', function () {
-  $('#mail-form').on('submit', function (e) {
-    e.preventDefault();
-    $('#form-sending').show(), $('#mail-form').hide(), $.ajax({
-      method: 'POST',
-      url: 'https://submit.carlgo11.com/',
-      dataType: 'text',
-      cache: !1,
-      data: $(this).serialize(),
-      success: function () {
-        $('#form-sending').hide(), $('#form-success').fadeIn().alert()
-      },
-      error: function () {
-        $('#form-sending').hide(), $('#form-error').fadeIn().alert()
-      }
-    })
-  })
+let c_token = null;
+window.onloadTurnstileCallback = function() {
+  turnstile.render('#captcha', {
+    sitekey: '0x4AAAAAAABavw7IIXAqu0yj',
+    callback: token => c_token = token
+  });
+};
+
+const mailForm = document.getElementById('mail-form');
+const sending = document.getElementById('form-sending');
+const sent = document.getElementById('form-success');
+
+mailForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  sending.style.display = 'block';
+  mailForm.style.display = 'none';
+
+  const data = {
+    'response': c_token,
+    'name': document.getElementById('name').value,
+    'email': document.getElementById('email').value,
+    'subject': document.getElementById('subject').value,
+    'message': document.getElementById('message').value
+  };
+  await fetch('https://mail.carlgo11.workers.dev', {
+    method: 'POST',
+
+    body: JSON.stringify(data)
+  });
+  sending.style.display = 'none';
+  sent.style.display = 'block';
 })
